4eb0a86eb16068295988a06f3b00260c94a9dd2c
Ties Robroek <tt_robroek@hotmail.com>
2015-06-16 16:56:58 +0200

    Now uses busstrees. Header defined in header.ini New input/output format. Now supports PICK operator.

---
+++ examples/tafels.f
@@ -0,0 +1,16 @@
+\documentclass{standalone}
+\usepackage{bussproofs}
+\usepackage{amssymb}
+\usepackage{latexsym}
+
+% This is the "centered" symbol
+\def\fCenter{{\mbox{\Large$\rightarrow$}}}
+
+% Optional to turn on the short abbreviations
+\EnableBpAbbreviations
+
+% \alwaysRootAtTop  % makes proofs upside down
+% \alwaysRootAtBottom % -- this is the default setting
+
+\begin{document}
+\thispagestyle{empty}
